03f6741c72cd902f97661c2854e9909752af84ce
cummer <wtwtw1110@gmail.com>
2022-08-10 18:01:42 +0900

    로그인 페이지 작업

--- front/pages/index.js
+++ front/pages/index.js
@@ -5,19 +5,7 @@ import LoginContainer from "../containers/LoginContainer";
 export default function Home() {
   return (
     <>
-      <div className="max-w-4xl m-auto flex justify-center items-center">
-        <div className="flex-1 w-6/12">
-          <div>sdafd</div>
-        </div>
-        <div className="flex-1 w-6/12 max-w-xs">
-          <div className="border flex flex-col">
-            <div className="img">
-              <img src="" alt="" />
-            </div>
-            <div className="input flex flex-col mt-[24px]">fgfgfgf</div>
-          </div>
-        </div>
-      </div>
+      <div>home</div>
     </>
   );
 }
